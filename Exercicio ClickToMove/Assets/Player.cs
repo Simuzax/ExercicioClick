67bcf3f7e2c129553aa5cbd0948aa4b075db22fe
Simuzax <simuzax@gmail.com>
2019-10-03 01:20:53 -0300

    1st

---
+++ Exercicio ClickToMove/Assets/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    public LayerMask clickable;
+    private NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, clickable))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+}
